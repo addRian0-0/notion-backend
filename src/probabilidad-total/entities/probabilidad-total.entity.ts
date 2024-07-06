62324ba9f8a6446a5e22ddafecaeb024d5040177
Sergio Arce <al2223034493@azc.uam.mx>
2024-07-06 13:56:50 -0600

    correcion de entidades

--- src/probabilidad-total/entities/probabilidad-total.entity.ts
+++ src/probabilidad-total/entities/probabilidad-total.entity.ts
@@ -1,2 +1,22 @@
-export class ProbabilidadTotal {
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { RiesgoParticion, RiesgoParticionSchema } from './riesgo-particion.entity'; // Ajusta la ruta según tu estructura de archivos
+import { IsPositive } from 'class-validator';
+
+@Schema()
+export class RiesgoTotal extends Document {
+
+    @Prop({
+        unique:true
+    })
+    @IsPositive()
+    id: number;
+
+    @Prop({ type: [{ type: RiesgoParticionSchema }] })
+    riesgosParciales: RiesgoParticion[];
+
+    // Otras propiedades del RiesgoTotal, si las hay
 }
+
+export const RiesgoTotalSchema = SchemaFactory.createForClass(RiesgoTotal);
+
--- src/probabilidad-total/entities/riesgo-particion.entity.ts
+++ src/probabilidad-total/entities/riesgo-particion.entity.ts
@@ -1,23 +1,32 @@
-import { IsInt, IsNumber, IsPositive, IsString, Max, Min, MinLength } from "class-validator";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { IsPositive, IsString, IsNumber, MinLength, Max, Min } from 'class-validator';
 
-export class RiesgoParticion {
+@Schema()
+export class RiesgoParticion extends Document {
 
+    @Prop({ required: true , unique: true })
     @IsPositive()
-    id: number; // independiente del mongo id
+    id: number;
 
+    @Prop({ required: true })
     @IsString()
     @MinLength(4)
     nombre: string;
 
+    @Prop({ required: true })
     @IsNumber()
     @IsPositive()
     @Max(100)
     @Min(0)
     impacto: number;
 
+    @Prop({ required: true })
     @IsNumber()
     @IsPositive()
     @Max(100)
     @Min(0)
     probabilidad: number;
 }
+
+export const RiesgoParticionSchema = SchemaFactory.createForClass(RiesgoParticion);
