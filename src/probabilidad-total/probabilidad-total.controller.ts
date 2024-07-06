176b260c2bde97bf7974ce579d937092a2a3421e
Sergio Arce <arce.roldan.sergio@gmail.com>
2024-07-06 15:27:28 -0600

    07:(seed)

--- src/probabilidad-total/probabilidad-total.controller.ts
+++ src/probabilidad-total/probabilidad-total.controller.ts
@@ -4,26 +4,68 @@ import { ProbabilidadTotalService } from './probabilidad-total.service';
 import { CreateProbabilidadTotalDto } from './dto/probabilidad-total/create-probabilidad-total.dto';
 import { UpdateProbabilidadTotalDto } from './dto/probabilidad-total/update-probabilidad-total.dto';
 
-@Controller('probabilidad-total')
+@Controller('riesgos')
 export class ProbabilidadTotalController {
   constructor(private readonly probabilidadTotalService: ProbabilidadTotalService) {}
 
-  @Post()
-  create(@Body() createProbabilidadTotalDto: CreateProbabilidadTotalDto) {
-    return this.probabilidadTotalService.create(createProbabilidadTotalDto);
-  }
+
+  riesgos = [
+    {
+      "id": 1,
+      "nombre": "Riesgo Financiero",
+      "impacto": 75,
+      "probabilidad": 60
+    },
+    {
+      "id": 2,
+      "nombre": "Riesgo Operacional",
+      "impacto": 50,
+      "probabilidad": 45
+    },
+    {
+      "id": 3,
+      "nombre": "Riesgo Tecnológico",
+      "impacto": 90,
+      "probabilidad": 30
+    },
+    {
+      "id": 4,
+      "nombre": "Riesgo Legal",
+      "impacto": 40,
+      "probabilidad": 20
+    },
+    {
+      "id": 5,
+      "nombre": "Riesgo Reputacional",
+      "impacto": 65,
+      "probabilidad": 55
+    }
+]; 
+
+  // @Post()
+  // create(@Body() createProbabilidadTotalDto: CreateProbabilidadTotalDto) {
+  //   this.riesgo.push( createProbabilidadTotalDto )
+  // }
 
   @Get()
   findAll() {
-    return this.probabilidadTotalService.findAll();
+    let riesgoPonderado: number;
+    
+    for (const riesgo of this.riesgos) {
+      riesgoPonderado = +riesgo.probabilidad
+    }
+    
+    return this.riesgos;
   }
 
-  @Get(':id')
+  @Get('riesgo/:id')
   findOne(@Param('id') id: string) {
-    return this.probabilidadTotalService.findOne(+id);
+    
+    // const riesgoInteres = this.riesgos.find( riesgo => id === riesgo.id ); 
+    // return riesgoInteres;
   }
 
-  @Patch(':id')
+  @Patch('riesgo/:id')
   update(@Param('id') id: string, @Body() updateProbabilidadTotalDto: UpdateProbabilidadTotalDto) {
     return this.probabilidadTotalService.update(+id, updateProbabilidadTotalDto);
   }
